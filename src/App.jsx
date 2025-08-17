c3c764ee877a28c0768e7a1be7768b3ab1c087bb
akshay girase <akshaygirase122003@gmail.com>
2025-08-17 20:01:04 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import anzo from './assets/anzo.avif'
+
 import Page1 from './pages/Page1'
 import Header from './components/Header'
 import Page2 from './pages/Page2'
